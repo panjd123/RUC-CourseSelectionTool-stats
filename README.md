de9333a90f50d6365c3d9e6a55004a9296119b02
panjd123 <1747366367@qq.com>
2025-02-21 14:56:21 +0800

    Update

---
+++ README.md
@@ -0,0 +1,3 @@
+# RUC-CourseSelectionTool-stats
+
+本仓库是 [RUC-CourseSelectionTool](https://github.com/panjd123/RUC-CourseSelectionTool) 工具对应的统计服务器。
--- main.py
+++ main.py
@@ -136,4 +136,4 @@ async def root():
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="127.0.0.1", port=10802, workers=1)
+    uvicorn.run(app, host="127.0.0.1", port=10882, workers=1)
